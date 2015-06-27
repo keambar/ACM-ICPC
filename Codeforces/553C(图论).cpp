c96a6ab2eee090c7e9b6afdd3853c2b2b6204ab8
keambar <keambar@gmail.com>
2015-06-28 00:33:08 +0800

    6.28.2015

--- Codeforces/553C(图论).cpp
+++ Codeforces/553C(图论).cpp
@@ -1,23 +1,55 @@
 #include <bits/stdc++.h>
-#define FO(i,a,b) for (int i = (a); i < (b); i++)
-int n,m,x,y,t,c[100005];
-std::vector<int> a[100005],b[100005];
-void g(int i, int d) {
-	if (c[i] && c[i] != d) printf("0\n"), exit(0);
-	else if (!c[i]) {
-		c[i] = d;
-		for (int x : a[i]) g(x,d);
-		for (int x : b[i]) g(x,3-d);
-	}
+
+using namespace std;
+const int N = 100009;
+typedef pair<int, int> ii;
+int  vis[N], n, m , flag , s;
+vector<ii> E[N];
+
+void dfs (int x, int k)
+{
+    vis[x] = k;
+    --s;
+    for (auto &i : E[x]) {
+        if (!~vis[i.first]) {
+            dfs (i.first, k ^ i.second);
+        } else {
+            if ( (vis[x]^vis[i.first]) != i.second) {
+                flag = 1;
+                return ;
+            }
+        }
+        if (flag) return;
+    }
 }
-int main() {
-	scanf("%d%d",&n,&m);
-	FO(i,0,m) {
-		scanf("%d%d%d", &x, &y, &t);
-		(t?a[x]:b[x]).push_back(y);
-		(t?a[y]:b[y]).push_back(x);
-	}
-	int r = 500000004;
-	FO(i,1,n+1) if (!c[i]) g(i,1), r = r*2%1000000007;
-	printf("%d\n", r);
+
+int main()
+{
+    memset (vis, -1, sizeof vis);
+    ios::sync_with_stdio ( 0 );
+    cin >> n >> m;
+    s = n;
+    for ( int i = 1, u, v, c; i <= m; ++i ) {
+        cin >> u >> v >> c;
+        E[u].push_back (make_pair (v, c ^ 1) );
+        E[v].push_back (make_pair (u, c ^ 1) );
+    }
+    for (int i = 1; i <= n; ++i) {
+        if (!~vis[i] && !E[i].empty() ) {
+            ++s;
+            dfs (i, 1);
+        }
+        if (flag) {
+            cout << 0 << endl;
+            return 0;
+        }
+    }
+    long long  ans = 1, p = 2;
+    --s;
+    while ( s > 0 ) {
+        if ( s & 1 ) ans = ans * p % 1000000007;
+        p = ( p * p ) % 1000000007;
+        s >>= 1;
+    }
+    cout << ans << endl;
 }
