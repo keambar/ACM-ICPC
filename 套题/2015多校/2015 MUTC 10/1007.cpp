981f0dc8baa827d6348de367ef635cbd4f0dbe37
keambar <keambar@gmail.com>
2015-08-21 15:58:18 +0800

    8.21.2015

--- 套题/2015多校/2015 MUTC 10/1007.cpp
+++ 套题/2015多校/2015 MUTC 10/1007.cpp
@@ -1,164 +1,137 @@
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-#include<cmath>
-#include<cstring>
-#define maxn 400005
-#define inf 1000000000
-
+#include <bits/stdc++.h>
 using namespace std;
-struct query {
-    int x, y, k, s, tp, cur;
-} q[maxn], q1[maxn], q2[maxn];
-int a[maxn], ans[maxn], tmp[maxn], t[maxn];
-int n, m, num, cnt;
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+#define LL long long
+#define se second
+#define fi first
+const int N = 200005;
+const int Mod = ( int ) 1e9 + 7;
 
 inline void rd( int &xx )
 {
     char ch = getchar();
-    while( ch < '0' || ch > '9' ) {
-        ch = getchar();
-    }
-    for( xx = 0; ch >= '0' && ch <= '9'; ch = getchar() ) {
+    while( ch < '0' || ch > '9' ) ch = getchar();
+    for( xx = 0; ch >= '0' && ch <= '9'; ch = getchar() )
         xx = xx * 10 + ( ch - '0' );
-    }
 }
 char o[35];
-
 inline void putnum( int x )
 {
     if( x == 0 ) {
-        putchar( '0' );
+        puts( "0" );
         return;
     }
     int i;
-    for( i = 0; x != 0; i++, x /= 10 ) {
-        o[i] = x % 10 + '0' ;
-    }
-    for( i--; i >= 0; i-- ) {
-        putchar( o[i] );
-    }
+    for( i = 0; x != 0; i++, x /= 10 ) o[i] = x % 10 + '0' ;
+    for( i--; i >= 0; i-- ) putchar( o[i] );
     putchar( 10 );
 }
-void add( int x, int y )
+
+struct node {
+    int sta, x, y, k, sum, tmp, id;
+    node() {};
+    node( int a, int b, int c, int d ): sta( a ), x( b ), y( c ), k( d )
+    {
+        sum = 0, tmp = 0;
+    };
+} q[N << 1], q1[N << 1], q2[N << 1];
+int cnt, num;
+
+int n;
+int a[N], ans[N];
+
+int S[N];
+inline void add( int x, int k )
 {
-    for( int i = x; i <= n; i += ( i & -i ) ) {
-        t[i] += y;
-    }
+    for( ; x <= n; x += x & -x ) S[x] += k;
 }
 
-int ask( int x )
+inline int query( int x )
 {
-    int tmp = 0;
-    for( int i = x; i > 0; i -= ( i & -i ) ) {
-        tmp += t[i];
-    }
-    return tmp;
+    int res = 0;
+    for( ; x > 0; x -= x & -x ) res += S[x];
+    return res;
 }
 
-void divide( int head, int tail, int l, int r )
+void Solve( int L, int R, int l, int r )
 {
-    //cout<<head<<' '<<tail<<' '<<l<<' '<<r<<endl;
-    if( head > tail ) {
-        return ;
-    }
+    if( L > R ) return ;
     if( l == r ) {
-        for( int i = head; i <= tail; i++ )
-            if( q[i].tp == 3 ) {
-                ans[q[i].s] = l;    //,cout<<l<<endl;
+        for( int i = L; i <= R; ++i ) {
+            if( q[i].sta == 3 ) {
+                ans[q[i].id] = l;
             }
+        }
         return ;
     }
     int mid = ( l + r ) >> 1;
-    for( int i = head; i <= tail; i++ ) {
-        if( q[i].tp == 1 && q[i].y <= mid ) {
-            add( q[i].x, 1 );
-        } else
-            if( q[i].tp == 2 && q[i].y <= mid ) {
-                add( q[i].x, -1 );
-            } else
-                if( q[i].tp == 3 ) {
-                    tmp[i] = ask( q[i].y ) - ask( q[i].x - 1 );
-                }
+    for( int i = L; i <= R; ++i ) {
+        if( q[i].sta == 1 && q[i].y <= mid ) add( q[i].x, 1 );
+        if( q[i].sta == 2 && q[i].y <= mid ) add( q[i].x, -1 );
+        if( q[i].sta == 3 ) {
+            q[i].tmp = query( q[i].y ) - query( q[i].x - 1 );
+        }
     }
-    for( int i = head; i <= tail; i++ ) {
-        if( q[i].tp == 1 && q[i].y <= mid ) {
-            add( q[i].x, -1 );
-        } else
-            if( q[i].tp == 2 && q[i].y <= mid ) {
-                add( q[i].x, 1 );
-            }
+    for( int i = L; i <= R; ++i ) {
+        if( q[i].sta == 1 && q[i].y <= mid ) add( q[i].x, -1 );
+        if( q[i].sta == 2 && q[i].y <= mid ) add( q[i].x, +1 );
     }
     int l1 = 0, l2 = 0;
-    for( int i = head; i <= tail; i++ )
-        if( q[i].tp == 3 ) {
-            if( q[i].cur + tmp[i] > q[i].k - 1 ) { //q[i].cur+tmp[i]表示累积了多少个数
-                q1[++l1] = q[i];
-            } else {
-                q[i].cur += tmp[i];
+    for( int i = L; i <= R; ++i ) {
+        if( q[i].sta == 3 ) {
+            if( q[i].sum + q[i].tmp < q[i].k ) {
+                q[i].sum += q[i].tmp;
                 q2[++l2] = q[i];
+            } else {
+                q1[++l1] = q[i];
             }
         } else {
             if( q[i].y <= mid ) {
                 q1[++l1] = q[i];
-            } else {
+            } else
                 q2[++l2] = q[i];
-            }
         }
-    for( int i = 1; i <= l1; i++ ) {
-        q[head + i - 1] = q1[i];
     }
-    for( int i = 1; i <= l2; i++ ) {
-        q[head + l1 + i - 1] = q2[i];
+    for( int i = 1; i <= l1; ++i ) {
+        q[L + i - 1] = q1[i];
     }
-    divide( head, head + l1 - 1, l, mid );
-    divide( head + l1, tail, mid + 1, r );
+    for( int i = 1; i <= l2; ++i ) {
+        q[L + l1 + i - 1] = q2[i];
+    }
+    Solve( L, L + l1 - 1, l, mid );
+    Solve( L + l1, R, mid + 1, r );
 }
-
 int main()
 {
-    while( ~scanf( "%d", &n ) ) {
-        memset( t, 0, sizeof t );
-        memset( ans, 0, sizeof ans );
-        memset( tmp, 0, sizeof tmp );
-        memset( q, 0, sizeof q );
-        memset( q1, 0, sizeof q1 );
-        memset( q2, 0, sizeof q2 );
+    while( scanf( "%d", &n ) != EOF ) {
         cnt = 0, num = 0;
-        for( int i = 1; i <= n; i++ ) {
+        for( int i = 1; i <= n; ++i ) {
             rd( a[i] );
-            q[++num].x = i;
-            q[num].y = a[i];
-            q[num].tp = 1;
-            q[num].s = 0;
+            q[++cnt] = node( 1, i, a[i], 0 );
         }
-        int cmd;
-        int x, y, z;
-        rd( m );
-        for( int i = 1; i <= m; i++ ) {
+        int m;
+        scanf( "%d", &m );
+        int cmd, x, y, k;
+        for( int i = 1; i <= m; ++i ) {
             rd( cmd );
-            if( cmd == 2 ) {
-                rd( x ), rd( y ), rd( z );
-                q[++num].x = x, q[num].y = y, q[num].k = z;
-                q[num].tp = 3;
-                q[num].s = ++cnt;
-            } else {
+            if( cmd == 1 ) {
                 rd( x ), rd( y );
-                q[++num].x = x;
-                q[num].y = a[x];
-                q[num].tp = 2;
-                q[num].s = 0;
-                q[++num].x = x;
-                q[num].y = y;
-                q[num].tp = 1;
-                q[num].s = 0;
+                q[++cnt] = node( 2, x, a[x], 0 );
+                q[++cnt] = node( 1, x, y, 0 );
                 a[x] = y;
+            } else {
+                rd( x ), rd( y ), rd( k );
+                q[++cnt] = node( 3, x, y, k );
+                q[cnt].id = ++num;
             }
         }
-        divide( 1, num, 0, inf );
-        for( int i = 1; i <= cnt; i++ ) {
-            putnum( ans[i] );
+        Solve( 1, cnt, 1, int( 1e9 ) );
+        for( int i = 1; i <= num; ++i ) {
+            printf( "%d\n", ans[i] );
         }
     }
-    return 0;
 }
